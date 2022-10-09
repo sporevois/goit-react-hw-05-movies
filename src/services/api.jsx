dd4bb3ce476ba712f89a964ee55698cd21576f4c
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-10-09 13:27:22 +0300

    add api services

---
+++ src/services/api.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const KEY = '9c40587b7d16ffbdc73a57b7c7629b49';
+
+export const fetchTrending = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${KEY}`);
+  const data = response.data.results;
+  return data;
+};
+
+export const fetchMovie = async query => {
+  const response = await axios.get(
+    `search/movie?api_key=${KEY}&query=${query}`
+  );
+  const data = response.data.results;
+  return data;
+};
+
+export const fetchDetails = async id => {
+  const response = await axios.get(`movie/${id}?api_key=${KEY}`);
+  const data = response.data;
+  return data;
+};
+
+export const fetchCredits = async id => {
+  const response = await axios.get(`movie/${id}/credits?api_key=${KEY}`);
+  const data = response.data;
+  return data;
+};
+
+export const fetchReviews = async id => {
+  const response = await axios.get(`movie/${id}/reviews?api_key=${KEY}`);
+  const data = response.data;
+  return data;
+};
