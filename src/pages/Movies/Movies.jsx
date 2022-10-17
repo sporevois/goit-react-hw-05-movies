c5f1032c47dc55f2f703939adc7763f7c0231cc1
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-10-17 15:09:56 +0300

    add Error message on failed search

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link, useSearchParams, useLocation } from 'react-router-dom';
 import SearchBox from 'components/SearchBox/SearchBox';
 import { searchMovies } from 'services/api';
-import { Container} from 'utils/Container.styled';
+import { Container } from 'utils/Container.styled';
 import { ListItem } from 'pages/Home/Home.styled';
 
 const Movies = () => {
@@ -20,6 +20,11 @@ const Movies = () => {
     const getMoviesByQuery = async () => {
       try {
         const searchResults = await searchMovies(searchQuery);
+        if (searchResults.length === 0) {
+          throw new Error(
+            `${searchQuery} not found. Please, enter another request`
+          );
+        }
         setFoundMovies([...searchResults]);
       } catch (error) {
         setError(error.message);
